e2215c29fc31d10efd2456939d91f0f4e7fe0e77
xiang <xiang.tan@hpe.com>
2016-02-23 10:22:08 +0800

    finish plot4

--- figure/plot1.R
+++ figure/plot1.R
@@ -1,9 +1,6 @@
 plot1 <- function() {
-  conn <- file("household_power_consumption.txt", "r")
-  df <- sqldf("select * from conn where Date in ('1/2/2007', '2/2/2007')", file.format = list(header = TRUE, sep = ";"))
-  close(conn)
-  df[df == "?"] = NA
-  my_df <- tbl_df(df)
-  mutate(my_df, Date = as.Date(Date, "%d/%m/%Y"))
-  hist(my_df$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)")
+  df <- prepare()
+  png(filename = "plot1.png", bg = "transparent")
+  hist(df$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power(kilowatts)")
+  dev.off()
 }
--- figure/plot4.R
+++ figure/plot4.R
@@ -1,11 +1,15 @@
-plot3 <- function() {
+plot4 <- function() {
   df <- prepare()
+  png(filename = "plot4.png", bg = "transparent")
+  par(mfrow = c(2,2), mar = c(4, 4, 2, 1))
   x <- strptime(paste(df$Date, df$Time), "%d/%m/%Y %H:%M:%S")
-  png(file = "plot3.png", bg = "transparent")
+  plot(x, df$Global_active_power, type = "l", ylab = "Global Active Power", xlab = "")
+  plot(x, df$Voltage, type = "l", ylab = "Voltage", xlab = "datetime")
   plot(x, df$Sub_metering_1, type = "n", ylab = "Energy Sub metering", xlab = "")
   lines(x, df$Sub_metering_1, ylab = "Energy Sub metering", xlab = "")
   lines(x, df$Sub_metering_2, col = "red", ylab = "Energy Sub metering", xlab = "")
   lines(x, df$Sub_metering_3, col = "blue", ylab = "Energy Sub metering", xlab = "")
   legend("topright",col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1)
+  plot(x, df$Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = "datetime")
   dev.off()
 }
